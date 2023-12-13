7a254e0aefa1c113f34dccbbd2f757f6e33b9768
Zelyna Farrell <zsillas@yahoo.com>
2023-12-12 17:19:32 -0800

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Zelyna Sillas on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calendar
+//
+//  Created by Zelyna Sillas on 12/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
